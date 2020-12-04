947c38863b3aeb8a1107f291007c36b55e8c6a81
svetlana-yegorova <svetlana.yegorova@gmail.com>
2020-12-03 17:33:44 -0700

    Subsampled the dataset to get a more even representation across kms and mds, re-made plots with subsampled dataset

--- code/Tree_vs_biomass.R
+++ code/Tree_vs_biomass.R
@@ -9,7 +9,8 @@ trees_fia<-subset(data_out, !(is.na(analog_trees)))
 ### Libraries ################
 library(tidyverse)
 library(ggplot2)
-
+library(car)
+library(dplyr)
 
 ### check the normality assumption of the tree cover data 
 ### 1. tree cover data are not normally distributed, but follow more of a uniform distribution) 
@@ -129,9 +130,9 @@ trees_fia_s3$resid<-tr_tr3$residuals
 #2. Create a hexbin plot of residuals by climate and physical distance. 
 head(trees_fia_s3)
 
-residual_hexbin<-ggplot(data=trees_fia_s3, aes(x=km, y=MD, z=abs(resid)))+
+residual_hexbin<-ggplot(data=trees_fia_s3, aes(x=km, y=MD, z=abs(tree_resid)))+
 stat_summary_hex(fun="mean", bins=50)+
-viridis::scale_fill_viridis(limits  = c(0, 50),na.value='yellow',name='mean \nabs(residual)\n') +
+viridis::scale_fill_viridis(limits  = c(0, 30),na.value='red',name='mean \nabs(residual)\n') +
 ggtitle("Residuals from analog ~ focal tree cover mapped by climatic \nand physical distance. (Sigma<=3)\n")+
 theme_bw()+
 theme(plot.title = element_text(color = "black", size = 14, face = "bold", hjust = 0.5),
@@ -182,7 +183,7 @@ ratio_hexbin
 
 
 data_density<-ggplot(data=trees_fia_s3, aes(x=km, y=MD))+
-  stat_binhex(aes(fill=log10(..count..)),bins=30) +
+  stat_binhex(aes(fill=log10(..count..)),bins=50) +
   viridis::scale_fill_viridis(limits  = c(0, 3.5),na.value='red',name='log10 \n(# observations)\n') +
   ggtitle("Density of observations. (Sigma<=3)")+
   theme_bw()+
@@ -192,17 +193,17 @@ data_density<-ggplot(data=trees_fia_s3, aes(x=km, y=MD))+
         axis.title.x = element_text(size = 15),-
         axis.title.y = element_text(size = 15))
 data_density
-png("./output/observation_density.png")
+png("./outputs/observation_density.png")
 data_density
 dev.off()
 
 
 #3c. Look at the variability of each hexbin (vs mean estimate that we looked at above)
 
-variability<-ggplot(data=trees_fia_s3, aes(x=km, y=MD, z=tree_ratio))+
-  stat_summary_hex(fun=function(z) sd(z), bins=100)+
-  viridis::scale_fill_viridis(limits  = c(0, 5),na.value='yellow',name='sd(ratio)') +
-  ggtitle("ratio variability by climatic and physical distance. (Sigma<=3)")+
+variability<-ggplot(data=subset(trees_fia_s3, focal_trees>=1), aes(x=km, y=MD, z=tree_ratio))+
+  stat_summary_hex(fun=function(z) sd(z), bins=50)+
+  viridis::scale_fill_viridis(limits  = c(0, 10),na.value='red',name='sd(ratio)') +
+  ggtitle("ratio variability by climatic and physical distance. (Sigma<=3)", "focal pixels with <1% cover excluded")+
   theme_bw()+
   theme(plot.title = element_text(color = "black", size = 14, face = "bold", hjust = 0.5),
         plot.subtitle = element_text(color = "gray41", size = 14, face = "bold", hjust = 0.5),
@@ -215,9 +216,9 @@ dev.off()
 
 #3d. Look at the variability of residuals in each hexbin (vs ratio looked at above)
 
-resid_variability<-ggplot(data=trees_fia_s3, aes(x=km, y=MD, z=tree_resid))+
+resid_variability<-ggplot(data=subset(trees_fia_s3, focal_trees>=1), aes(x=km, y=MD, z=abs(tree_resid)))+
   stat_summary_hex(fun=function(z) sd(z), bins=50)+
-  viridis::scale_fill_viridis(limits  = c(0, 30),na.value='red',name='sd(ratio)') +
+  viridis::scale_fill_viridis(limits  = c(0, 20),na.value='red',name='sd(residual)') +
   ggtitle("residual variability by climatic and physical distance. (Sigma<=3)")+
   theme_bw()+
   theme(plot.title = element_text(color = "black", size = 14, face = "bold", hjust = 0.5),
@@ -225,6 +226,143 @@ resid_variability<-ggplot(data=trees_fia_s3, aes(x=km, y=MD, z=tree_resid))+
         plot.caption = element_text(color = "gray65", face = "italic"),
         axis.title.x = element_text(size = 15),
         axis.title.y = element_text(size = 15))
-png("./outputs/ratio_variability.png")
+png("./outputs/residual_variability.png")
 resid_variability
 dev.off()
+
+
+# plot residuals by MD: 
+
+ggplot(data=trees_fia_s3, aes(x=MD, y=abs(tree_resid)))+
+  stat_binhex(aes(fill=log10(..count..)),bins=50)+
+  geom_abline(intercept=res_md$coefficients[1], slope=res_md$coefficients[2])
+
+res_md<-lm(abs(tree_resid)~MD+km, data=trees_fia_s3)
+summary(res_md)
+
+res_km_md<-lm(abs(tree_resid)~km +MD +km*MD, data=trees_fia_s3)
+summary(res_km_md)
+
+res_km<-lm(abs(tree_resid)~km, data=trees_fia_s3)
+summary(res_km)
+vif(res_km_md)
+
+
+#################### can I subsample my data based on analog distance? 
+
+head(trees_fia_s3)
+
+# what are the values of km_bin? 
+unique(trees_fia_s3$km_bin)
+
+# how many observations per bin? 
+hist(trees_fia_s3$km_bin)
+abline(a=1000,b=0, col='red')
+
+
+# subsample to 853 observations: the number of observations in the last kilometer bin
+nrow(subset(trees_fia_s3, km_bin==1500))
+
+trees_sub853 <- subset(trees_fia_s3, km_bin<=1450) %>% group_by(km_bin) %>% sample_n(853)
+head(trees_sub853)
+
+hist(trees_sub853$km_bin)
+
+# subsample again, but 0.25 sigma level: 
+trees_sub853$sigma_level<-floor(trees_sub853$Sigma/0.25)
+trees_sub<-trees_sub853%>%group_by(sigma_level)%>%sample_n(1000)
+
+
+#look at data density now: 
+
+data_density<-ggplot(data=trees_sub, aes(x=km, y=MD))+
+  stat_binhex(aes(fill=log10(..count..)),bins=50) +
+  viridis::scale_fill_viridis(limits  = c(0, 2),na.value='red',name='log10 \n(# observations)\n') +
+  ggtitle("Density of observations. (Sigma<=3)")+
+  theme_bw()+
+  theme(plot.title = element_text(color = "black", size = 14, face = "bold", hjust = 0.5),
+        plot.subtitle = element_text(color = "gray41", size = 14, face = "bold", hjust = 0.5),
+        plot.caption = element_text(color = "gray65", face = "italic"),
+        axis.title.x = element_text(size = 15),
+        axis.title.y = element_text(size = 15))
+data_density
+
+png("./outputs/subsampled_density.png")
+data_density
+dev.off()
+
+# now re-plot residual variability with the subset: 
+
+resid_variability<-ggplot(data=subset(trees_sub, focal_trees>=1),  aes(x=km, y=MD, z=abs(tree_resid)))+
+  stat_summary_hex(fun=function(z) sd(z), bins=30)+
+  viridis::scale_fill_viridis(limits  = c(0, 25),na.value='red',name='sd(residual)') +
+  ggtitle("residual variability by climatic and physical distance. (Sigma<=3)")+
+  theme_bw()+
+  theme(plot.title = element_text(color = "black", size = 14, face = "bold", hjust = 0.5),
+        plot.subtitle = element_text(color = "gray41", size = 14, face = "bold", hjust = 0.5),
+        plot.caption = element_text(color = "gray65", face = "italic"),
+        axis.title.x = element_text(size = 15),
+        axis.title.y = element_text(size = 15))
+
+png("./outputs/sub_residual_variability.png")
+resid_variability
+dev.off()
+
+
+# now re-plot ratio variability with the subset: 
+
+ratio_variability<-ggplot(data=subset(trees_sub, focal_trees>=1),  aes(x=km, y=MD, z=tree_ratio))+
+  stat_summary_hex(fun=function(z) sd(z), bins=30)+
+  viridis::scale_fill_viridis(limits  = c(0, 5),na.value='grey',name='sd(residual)') +
+  ggtitle("ratio variability by climatic and physical distance. (Sigma<=3)")+
+  theme_bw()+
+  theme(plot.title = element_text(color = "black", size = 14, face = "bold", hjust = 0.5),
+        plot.subtitle = element_text(color = "gray41", size = 14, face = "bold", hjust = 0.5),
+        plot.caption = element_text(color = "gray65", face = "italic"),
+        axis.title.x = element_text(size = 15),
+        axis.title.y = element_text(size = 15))
+
+png("./outputs/sub_ratio_variability.png")
+ratio_variability
+dev.off()
+
+# mean ratios: 
+ratio_hexbin<-ggplot(data=subset(trees_sub, focal_trees>=1), aes(x=km, y=MD, z=tree_ratio))+
+  stat_summary_hex(fun="mean", bins=30)+
+  viridis::scale_fill_viridis(limits  = c(0, 3),na.value='grey',name='mean \nratio\n') +
+  ggtitle("difference ratio mapped by climatic \nand physical distance. (Sigma<=3)\n", "focal pixels with tree cover < 1% excluded")+
+  theme_bw()+
+  theme(plot.title = element_text(color = "black", size = 14, face = "bold", hjust = 0.5),
+        plot.subtitle = element_text(color = "gray41", size = 14, face = "bold", hjust = 0.5),
+        plot.caption = element_text(color = "gray65", face = "italic"),
+        axis.title.x = element_text(size = 15),
+        axis.title.y = element_text(size = 15))
+png("./outputs/sub_mean_ratio.png")
+ratio_hexbin
+dev.off()
+
+
+# mean residuals: 
+
+resid_hexbin<-ggplot(data=trees_sub, aes(x=km, y=MD, z=abs(tree_resid)))+
+  stat_summary_hex(fun=function(z) mean(abs(z)), bins=30)+
+  viridis::scale_fill_viridis(limits  = c(0, 35),na.value='grey',name='mean \nresidual\n') +
+  ggtitle("sub-sampled residuals mapped by climatic \nand physical distance. (Sigma<=3)\n")+
+  theme_bw()+
+  theme(plot.title = element_text(color = "black", size = 14, face = "bold", hjust = 0.5),
+        plot.subtitle = element_text(color = "gray41", size = 14, face = "bold", hjust = 0.5),
+        plot.caption = element_text(color = "gray65", face = "italic"),
+        axis.title.x = element_text(size = 15),
+        axis.title.y = element_text(size = 15))
+
+png("./outputs/sub_residuals_byMD&km.png")
+resid_hexbin
+dev.off()
+
+
+ggplot(data=trees_sub, aes(x=MD, y=tree_resid))+
+  geom_point()
+
+
+ggplot(data=trees_sub, aes(x=focal_trees, y=analog_trees))+
+  geom_point()
